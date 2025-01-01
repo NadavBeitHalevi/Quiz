ff0760c04fa1d9e56c6e13b682427fd23954d655
Nadav Beit-Halevi <nadav.beithalevi@gmail.com>
2025-01-01 12:07:08 +0200

    update files

--- quiz_service.py
+++ quiz_service.py
@@ -4,24 +4,18 @@ import requests
 from question_model import Question
 
 class QuizService:
-    def __init__(self):
+    def __init__(self, base_url: str = "https://opentdb.com/api.php", question_type: str = "boolean"):
         self.parameters = {
-            "type": "boolean"
+            "type": question_type
         }
-        self.base_url = "https://opentdb.com/api.php"
+        self.base_url = base_url
 
-    def get_questions(self, num_of_questions: str) -> List[Dict[str, str]]:
-        self.parameters["amount"] = num_of_questions
+    def get_questions(self, num_of_questions: int) -> List[Dict[str, str]]:
+        self.parameters["amount"] = str(num_of_questions)
         response = requests.get(self.base_url, params=self.parameters)
         response.raise_for_status()
-        question_data = response.json()["results"]
+        question_data = response.json().get("results", [])
         return question_data
     
     def quiz_transform(self, question_data: List[Dict[str, str]]) -> List[Question]:
-        question_bank = []
-        for question in question_data:
-            question_text = question["question"]
-            question_answer = question["correct_answer"]
-            new_question = Question(question_text, question_answer)
-            question_bank.append(new_question)
-        return question_bank
+        return [Question(question["question"], question["correct_answer"]) for question in question_data]
--- quiz_util.py
+++ quiz_util.py
@@ -5,11 +5,5 @@ from question_model import Question
 class QuizUtil:
 
     @staticmethod
-    def quiz_transform(question_data: List[Dict[str, str]]) -> List[Question]:
-        question_bank = []
-        for question in question_data:
-            question_text = question["question"]
-            question_answer = question["correct_answer"]
-            new_question = Question(question_text, question_answer)
-            question_bank.append(new_question)
-        return question_bank
+    def quiz_transform(self, question_data: List[Dict[str, str]]) -> List[Question]:
+        return [Question(question["question"], question["correct_answer"]) for question in question_data]
